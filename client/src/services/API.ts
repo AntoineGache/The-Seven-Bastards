6e8f5b4c5ed6d34c4e2f0bf43a82c3c1badde595
antoinegache <antoinegache1@gmail.com>
2023-11-25 13:42:04 +0100

    TSB-1: FRONT: Init front end and add the API connection

---
+++ client/src/services/API.ts
@@ -0,0 +1,12 @@
+import 'regenerator-runtime/runtime';
+import axios from 'axios';
+
+//Do not modify this file please
+
+const BASE_URL = '';
+
+const API = axios.create({
+    baseURL: BASE_URL
+});
+
+export default API;
