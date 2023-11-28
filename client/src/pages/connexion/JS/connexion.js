4a395290f1caa5748ac8d317891a518fc908564f
flodlb <florianbdlb2@gmail.com>
2023-11-28 22:25:51 +0100

    TSB-1:FRONT: I have add a connections pages

---
+++ client/src/pages/connexion/JS/connexion.js
@@ -0,0 +1,6 @@
+function afficheridentifiant() {
+    var texte = document.getElementById("identifiant").value;
+}
+function affichermotdepasse() {
+    var texte = document.getElementById("motDePasse").value;
+}
