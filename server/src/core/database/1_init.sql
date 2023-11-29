8fcd0089f186c9dbe3cafcce8d3362300b921034
raphaelgruet <raphaelgruet@yahoo.fr>
2023-11-30 00:27:21 +0100

    TSB-3: DATABASE: Database init script

--- server/src/core/database/1_init.sql
+++ server/src/core/database/1_init.sql
@@ -6,11 +6,100 @@
 
 use sevenBastards;
 
-drop table test;
-create table test(
-    id numeric primary key,
-    numberTest varchar(50),
-    name varchar(50)
+DROP TABLE IF EXISTS administrateur;
+ALTER TABLE club
+    DROP FOREIGN KEY club_ibfk_1;
+DROP TABLE IF EXISTS concours;
+DROP TABLE IF EXISTS dessin;
+DROP TABLE IF EXISTS competiteur;
+DROP TABLE IF EXISTS directeur;
+DROP TABLE IF EXISTS evaluation;
+DROP TABLE IF EXISTS evaluateur;
+DROP TABLE IF EXISTS president;
+DROP TABLE IF EXISTS utilisateur;
+DROP TABLE IF EXISTS club;
+
+CREATE TABLE utilisateur(
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(50),
+    prenom VARCHAR(50),
+    adresse VARCHAR(100),
+    login VARCHAR(50),
+    password VARCHAR(64), #SHA256
+    date_licence DATE,
+    club INTEGER
 );
 
-select * from test;
+CREATE TABLE competiteur(
+    id INTEGER PRIMARY KEY,
+    date_premiere_participation DATE,
+    FOREIGN KEY (id) REFERENCES utilisateur(id)
+);
+
+CREATE TABLE president(
+    id INTEGER PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES utilisateur(id)
+);
+
+CREATE TABLE administrateur(
+    id INTEGER PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES utilisateur(id)
+);
+
+CREATE TABLE evaluateur(
+    id INTEGER PRIMARY KEY,
+    specialite VARCHAR(50),
+    FOREIGN KEY (id) REFERENCES utilisateur(id)
+);
+
+CREATE TABLE directeur(
+    id INTEGER PRIMARY KEY,
+    FOREIGN KEY (id) REFERENCES utilisateur(id)
+);
+
+CREATE TABLE club(
+    id INTEGER PRIMARY KEY,
+    nom VARCHAR(50),
+    adresse VARCHAR(100),
+    ville VARCHAR(50),
+    region VARCHAR(50),
+    departement VARCHAR(50),
+    telephone VARCHAR(10),
+    nombre_adherents INTEGER,
+    directeur INTEGER,
+    FOREIGN KEY (directeur) REFERENCES directeur(id)
+);
+
+CREATE TABLE dessin(
+    id INTEGER PRIMARY KEY,
+    commentaire VARCHAR(500),
+    classement INTEGER,
+    date_remise DATE,
+    lien VARCHAR(200),
+    competiteur INTEGER,
+    FOREIGN KEY (competiteur) REFERENCES competiteur(id)
+);
+
+CREATE TABLE concours(
+    id INTEGER PRIMARY KEY,
+    theme VARCHAR(100),
+    description VARCHAR(500),
+    date_debut DATE,
+    date_fin DATE,
+    etat VARCHAR(100),
+    president INTEGER,
+    FOREIGN KEY (president) REFERENCES president(id)
+);
+
+CREATE TABLE evaluation(
+    id INTEGER PRIMARY KEY,
+    commentaire VARCHAR(500),
+    note INTEGER,
+    date_evaluation DATE,
+    evaluateur INTEGER,
+    FOREIGN KEY (evaluateur) REFERENCES evaluateur(id)
+);
+
+ALTER TABLE utilisateur
+ADD CONSTRAINT fk_utilisateur_club
+FOREIGN KEY (club) REFERENCES club(id);
