6588db0ca1e9c0bdb9c22e82f8498ad561ad72b0
antoinegache <antoinegache1@gmail.com>
2023-12-03 18:29:12 +0100

    TSB-3: BACK: Secury and route init finish

--- server/src/api/middleware/security.ts
+++ server/src/api/middleware/security.ts
@@ -1,7 +1,6 @@
 
 import {NextFunction, Request, Response} from 'express';
-/*
-import {JWTUtils} from '../utils/JWT';
+import {JWTUtils} from '../../core/utils/JWTUtils';
 
 export const isAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
 	let token = req.headers.authorization?.split(' ')[1];
@@ -16,7 +15,7 @@ export const isAuthenticated = async (req: Request, res: Response, next: NextFun
 	}
 	
 	try {
-		req.user = JWTUtils.decode(token);
+		req.body.user = JWTUtils.decode(token);
 		
 		next();
 	} catch (err) {
@@ -32,7 +31,7 @@ export const isAuthenticated = async (req: Request, res: Response, next: NextFun
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
 	
-	if (!req.user.admin) {
+	if (!req.body.user.roles) {
 		return res.status(403).send({
 			success: false,
 			message: 'Access denied. You are not an admin.',
@@ -46,7 +45,7 @@ export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
 
 export const isUser = (req: Request, res: Response, next: NextFunction) => {
 	
-	if (req.user.admin) {
+	if (req.body.user.admin) {
 		return res.status(403).send({
 			success: false,
 			message: 'Access denied. You are an admin.',
@@ -57,4 +56,3 @@ export const isUser = (req: Request, res: Response, next: NextFunction) => {
 	
 	next();
 };
-*/
