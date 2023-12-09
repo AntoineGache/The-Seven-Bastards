b88e92a828210f7fe213ca46378c6edb2ec4e49f
antoinegache <antoinegache1@gmail.com>
2023-12-09 13:54:23 +0100

    TSB-10: FRONT: Login connect to backend

--- client/src/services/authAPI.ts
+++ client/src/services/authAPI.ts
@@ -6,22 +6,19 @@ export type SignInCredentials = {
 	password: string
 }
 
-export namespace AuthAPI {
-
-    export const signIn = async (credentials: SignInCredentials | null = null): Promise<string> => {
-		
-		const authToken = Cookies.get('authToken');
-		
-		if (credentials) {
-			return (await API.post('/auth/login', credentials))?.data?.token as string;
-		} else {
-			if (!authToken) throw new Error('No credentials provided and no authToken found in cookies');
-			return (await API.post('/auth/login'))?.data?.token as string;
-		}
-	};
+export const signIn = async (credentials: SignInCredentials | null = null): Promise<string> => {
 	
-	export const signOut = async () => {
-		return await API.get('/auth/logout');
-	};
-}
+	const authToken = Cookies.get('authToken');
+	
+	if (credentials) {
+		return (await API.post('/auth/login', credentials))?.data?.token as string;
+	} else {
+		if (!authToken) throw new Error('No credentials provided and no authToken found in cookies');
+		return (await API.post('/auth/login'))?.data?.token as string;
+	}
+};
+
+export const signOut = async () => {
+	return await API.get('/auth/logout');
+};
 
