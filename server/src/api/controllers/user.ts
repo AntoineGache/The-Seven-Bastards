d0656a30690857cccd0374236c395d8b11d52290
antoinegache <antoinegache1@gmail.com>
2023-12-09 13:53:49 +0100

    TSB-9: BACK: Login example

--- server/src/api/controllers/user.ts
+++ server/src/api/controllers/user.ts
@@ -1,9 +1,11 @@
 import {NextFunction, Request, Response} from 'express';
-
+import { JWTUtils } from '../../core/utils/JWTUtils';
+import { CustomError } from '../utils/custom-error';
+import { BCryptUtils } from '../utils/Bcrypt';
 
 export const mysql = require('../../app');
 
-export type loginData {
+export type loginData = {
 	identifiant: string,
 	password: string
 }
@@ -18,6 +20,51 @@ export const register = async (req: Request, res: Response, next: NextFunction) 
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
 
+	console.log(req.body)
+
+	/*
+	let token: string
+	//let token :string = req.header.authorization?.split(' ')[1];
+
+	let user = null
+
+	try {
+
+		if(token) {
+			const decoded = JWTUtils.decode(token);
+			const identifiant = decoded['identifiant'];
+
+			user = 0 //requête SQL avec identiifant dans la requête
+			
+			if(!user) {
+				next(new CustomError('User not found', 404));
+			}
+
+		}
+		else {
+			//Cas ou il n'y a pas de cookie
+			
+			//refaire une requête sql
+
+			//Utiliser Bcrypt pour comparer le password
+
+		}
+
+		if(user) {
+			const dataAccessToken = {
+				"identifiant": user, //Modif pour ajouter l'identifiant du user user.identifiant un truc du genre
+				"roles": user
+			}
+
+			const authToken = JWTUtils.generateToken(dataAccessToken, {expiresIn: '7d'});
+			
+			res.status(201).json({authToken})
+		}
+	} catch (error) {
+		next(error)
+	}
+
+	*/
 	//requête SQL
 	// Traitement 
 
