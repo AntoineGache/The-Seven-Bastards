037188c1c22ebc66a0c9f1f7a57d3fc247337c36
Salxs <shams.alexandre@reseau.eseo.fr>
2023-12-10 22:41:53 +0100

    TSB-111:FRONT:ON NOUS VOLE NOTRE TRAVAIL git add .git add .git add .git add .!

--- client/src/pages/eval/eval.js
+++ client/src/pages/eval/eval.js
@@ -1,10 +1,21 @@
 import API from "../../services/API";
-//Récupération des données sous le format json 
+
+function addElement(donne){
+  var newDiv = document.createElement("div");
+  var newContent = document.createTextNode();
+  newDiv.appendChild(newContent);
+
+  var currentDiv = document.getElementById("div1");
+  document.body.insertBefore(newDiv, currentDiv);
+}
 
 export const recupConcours = async () => {
     await API.get('/eval/recupconcours')
     .then(response => {
-            console.log(response);
+        console.log(response);
+        for (donne in response){
+
+        }
     })
     .catch(error => console.error('Erreur lors de la récupération des données: ', error))
 }
@@ -12,6 +23,3 @@ export const recupConcours = async () => {
 document.addEventListener("DOMContentLoaded", function(){
     recupConcours();
 });
-
-
-    
