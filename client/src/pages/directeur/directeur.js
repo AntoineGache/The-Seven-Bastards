24aacb4c5023bc4f27d8afbc19b99e8517ed5061
flodlb <florianbdlb2@gmail.com>
2023-12-10 12:04:50 +0100

    TSB-8: Flo update president et attribuer avec page dinamique

--- client/src/pages/directeur/directeur.js
+++ client/src/pages/directeur/directeur.js
@@ -58,18 +58,8 @@ function creerTableau(tableauDeValeur) {
 }
 
 function inscription() {
-    /*chatgtp*/
-    // Récupérer les références des boutons
-    var showButton = document.getElementById('inscription');
-    // Vérifier l'état actuel du bouton caché
-    if (showButton.style.display === 'none') {
-        // Afficher le bouton caché
-        showButton.style.display = 'block';
-    } else {
-        // Masquer le bouton caché
-        showButton.style.display = 'none';
-    }
-    /**/
+    window.location.href = "../inscription/inscription.html";
+
 }
 
 
