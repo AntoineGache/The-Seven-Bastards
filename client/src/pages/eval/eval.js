de42b4311ad4c456eef7a39863ec37ae1bd8f189
Salxs <shams.alexandre@reseau.eseo.fr>
2023-12-11 15:44:55 +0100

    Modif sur eval.js

--- client/src/pages/eval/eval.js
+++ client/src/pages/eval/eval.js
@@ -3,6 +3,7 @@ import API from "../../services/API";
 
 export const recupConcours = async () => {
     await API.get('/eval/recupconcours')
+    let tab = []
     .then(response => {
         let containerGauche = document.getElementById("listConcours")
         const list = document.createElement("ul");
@@ -14,6 +15,7 @@ export const recupConcours = async () => {
             button.id = `btn${i}`;
             button.innerText = `Concours n°${i+1}`
             rowcontainer.appendChild(button)
+            tab.push(document.querySelector(`btn${i}`).addEventListener('click', concoursDetail(i)))
             let evalButton = document.createElement("button")
             evalButton.id = `evalbtn${i}`;
             evalButton.innerText = "Evaluer"
@@ -45,30 +47,30 @@ export const recupConcours = async () => {
         row.appendChild(cell)
 
         tblHead.appendChild(row)
-
-        for(let i = 0; i < response.data.length; i ++) {
-            row = document.createElement("tr");
-            cell = document.createElement("td");
-            cell.innerText = response.data[i].theme
-            row.appendChild(cell)
-            cell = document.createElement("td");
-            cell.innerHTML = response.data[i].description
-            row.appendChild(cell)
-            cell = document.createElement("td");
-            cell.innerHTML = response.data[i].etat
-            row.appendChild(cell)
-            cell = document.createElement("td");
-            cell.innerHTML = response.data[i].date_debut
-            row.appendChild(cell)
-            cell = document.createElement("td");
-            cell.innerHTML = response.data[i].date_fin
-            row.appendChild(cell)
-            tblBody.appendChild(row)
-        }
-        tbl.appendChild(tblHead);
-        tbl.appendChild(tblBody);
-        container.appendChild(tbl)
     })
     .catch(error => console.error('Erreur lors de la récupération des données: ', error))
 }
 window.onload = recupConcours();
+
+function concoursDetail (nombre){
+    row = document.createElement("tr");
+    cell = document.createElement("td");
+    cell.innerText = response.data[nombre-1].theme
+    row.appendChild(cell)
+    cell = document.createElement("td");
+    cell.innerHTML = response.data[nombre-1].description
+    row.appendChild(cell)
+    cell = document.createElement("td");
+    cell.innerHTML = response.data[nombre-1].etat
+    row.appendChild(cell)
+    cell = document.createElement("td");
+    cell.innerHTML = response.data[nombre-1].date_debut
+    row.appendChild(cell)
+    cell = document.createElement("td");
+    cell.innerHTML = response.data[nombre-1].date_fin
+    row.appendChild(cell)
+    tblBody.appendChild(row)
+    tbl.appendChild(tblHead);
+    tbl.appendChild(tblBody);
+    container.appendChild(tbl);
+}
